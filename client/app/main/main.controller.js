5b798baf78c2ceb0d7ddd2b6d3e40e2e2334848b
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-01-31 17:13:51 +0300

    Started with mockup.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,11 +6,20 @@ angular.module('projectsApp')
 
     $http.get('/api/measurs').success(function(measurs) {
       $scope.measurs = measurs;
-      socket.syncUpdates('measur', $scope.measurs);
+      if (measurs.length > 0) {
+        $scope.latestMeasur = measurs[measurs.length - 1];
+      }
+      socket.syncUpdates('measur', $scope.measurs, onNewMeasur);
     });
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('measur');
     });
+
+    function onNewMeasur(socketEvent, measur) {
+      if (socketEvent == 'created') {
+        $scope.latestMeasur = measur;
+      }
+    }
 
   });
