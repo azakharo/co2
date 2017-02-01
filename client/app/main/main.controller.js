87baa0e90c158c6f8c0ccff124d16728e2bad782
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-02-01 15:20:14 +0300

    Disp data on the chart.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,12 +3,24 @@
 angular.module('projectsApp')
   .controller('MainCtrl', function ($scope, $http, $timeout, socket) {
     $scope.measurs = [];
+    $scope.chartTimeVals = [];
+    $scope.chartTempVals = [];
+    $scope.chartCo2Vals = [];
 
     $http.get('/api/measurs').success(function(measurs) {
       $scope.measurs = measurs;
       if (measurs.length > 0) {
         $scope.latestMeasur = measurs[measurs.length - 1];
       }
+
+      // Prepare chart data
+      $scope.chartTimeVals = _.map(measurs, function(m){
+        return moment(m.timestamp).format('HH:mm');
+      });
+      $scope.chartCo2Vals = _.map(measurs, 'co2');
+      $scope.chartTempVals = _.map(measurs, 't');
+      drawChart();
+
       socket.syncUpdates('measur', $scope.measurs, onNewMeasur);
     });
 
@@ -26,58 +38,81 @@ angular.module('projectsApp')
     ///////////////////////////////////////////////////////////////////////////
     // Chart
 
-    $scope.chartConfig = {
-      options: {
-        title: {
-          text: 'Monthly Average Temperature',
-          x: -20 //center
-        },
-        subtitle: {
-          text: 'Source: WorldClimate.com',
-          x: -20
-        },
-        xAxis: {
-          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: {
+    function drawChart() {
+      $scope.chartConfig = {
+        options: {
           title: {
-            text: 'Temperature (°C)'
+            text: '',
+            x: -20 //center
           },
-          plotLines: [{
-            value: 0,
-            width: 1,
-            color: '#808080'
-          }]
+          credits: {
+            enabled: false
+          },
+          xAxis: {
+            categories: $scope.chartTimeVals,
+            crosshair: true
+          },
+          yAxis: [
+            { // co2 yAxis
+              labels: {
+                format: '{value}',
+                style: {
+                  color: 'red'
+                }
+              },
+              title: {
+                text: 'CO2, ppm',
+                style: {
+                  color: 'red'
+                }
+              },
+              allowDecimals: false
+            },
+            { // Temp yAxis
+              title: {
+                text: 'Температура, град.Цельсия'
+                //style: {
+                //  color: Highcharts.getOptions().colors[1]
+                //}
+              },
+              labels: {
+                format: '{value}'
+                //style: {
+                //  color: Highcharts.getOptions().colors[1]
+                //}
+              },
+              allowDecimals: true,
+              opposite: true,
+              min: 18,
+              max: 30,
+              gridLineWidth: 0
+            }
+          ],
+          tooltip: {
+            shared: true
+          },
+          legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+          }
         },
-        tooltip: {
-          valueSuffix: '°C'
-        },
-        legend: {
-          layout: 'vertical',
-          align: 'right',
-          verticalAlign: 'middle',
-          borderWidth: 0
+        series: [{
+          name: 'CO2',
+          data: $scope.chartCo2Vals,
+          yAxis: 0
+        }, {
+          name: 'temp',
+          data: $scope.chartTempVals,
+          yAxis: 1
+        }],
+        size: {
+          width: getChartW(),
+          height: getChartH()
         }
-      },
-      series: [{
-        name: 'Tokyo',
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-      }, {
-        name: 'New York',
-        data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-      }, {
-        name: 'Berlin',
-        data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-      }, {
-        name: 'London',
-        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-      }],
-      size: {
-        width: getChartW(),
-        height: getChartH()
-      }
-    };
+      };
+    }
 
     function updateChartSize() {
       $scope.chartConfig.size.width = getChartW();
